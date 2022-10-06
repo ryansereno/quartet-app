a08f5ed9d47fd6c9680310957712bc8be60d028e
R <ryansereno@protonmail.com>
2022-10-06 19:25:02 -0400

    added test product list generator

--- src/components/Recommendations/RecommendationList.js
+++ src/components/Recommendations/RecommendationList.js
@@ -1,14 +1,22 @@
 import ProductItem from "./ProductItem";
 import Card from "../UI/Card";
 import classes from "./RecommendationList.module.css"
-
-const products = [{ name: "OG Kush Flower", price: 23.0 }];
+const names=['100mg CBD Tincture', '1:1 THC/CBD Vaporizer', 'Strawnana Vaporizer', 'Blueberry Haze Flower', '10mg CBD Gummies', '2:1 THC/CBG Lozenges']
+let products = [] 
+for ( let i = 0; i<7; i++ ){
+  const newProduct = {
+    name:names[i],
+    price: Math.random()*50
+  }
+ products.push(newProduct)
+}
 const RecommendationList = (props) => {
   const productsList = products.map((product) => {
     return <ProductItem key={product.name} itemName={product.name} price={product.price} />;
   });
   return (
     <Card>
+
       <ul>{productsList}</ul>
     </Card>
   );
